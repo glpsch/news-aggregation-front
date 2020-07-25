ed6e2359fae8e3921f5f37f96528ab9eb27f2767
Galina Psch <over.passover@gmail.com>
2020-07-25 05:39:52 +0300

    popup open/close

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
-// ORIGINAL CONFIG
+"use strict";
+
 import '../pages/index.css';
 import '../pages/articles.css';
 
+import Popup from './js/components/Popup';
+
+
+// popup_registration
+
+(function () {
+
+const popupLoginElement = document.querySelector('.popup_login');
+const authBtn = document.querySelector('.header__button');
+const popupLogin = new Popup(popupLoginElement);
+authBtn.addEventListener('click', popupLogin.open.bind(popupLogin));
+
+
+
+
+
+// authBtn.addEventListener('click', function() {console.log('bubu')});
+
+// authBtn.addEventListener('click', undefined);
+
+
+
+// console.log('popupLogin')
+
+
+// console.log('scope:', this);
+})();
