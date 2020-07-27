bf2727abd654c9365aaad6ac02dc529003734e9e
Galina Psch <over.passover@gmail.com>
2020-07-27 22:51:29 +0300

    prettier

--- src/index.js
+++ src/index.js
@@ -1,59 +1,53 @@
 "use strict";
 
-import '../pages/index.css';
-import '../pages/articles.css';
+import "../pages/index.css";
+import "../pages/articles.css";
 
-import Popup from './js/components/Popup';
-
+import Popup from "./js/components/Popup";
 
 (function () {
+  const template = document.querySelector(".popup-template");
+  const popupLoginContent = document.querySelector(".popup_login__content");
+  const authBtn = document.querySelector(".header__button");
 
-const template = document.querySelector('.popup-template');
-const popupLoginContent = document.querySelector('.popup_login__content');
-const authBtn = document.querySelector('.header__button');
+  const popup = new Popup(template);
 
-const popup = new Popup(template);
+  authBtn.addEventListener("click", function () {
+    // console.log('got click')
+    if (!popup.isOpen) {
+      popup.setContent(popupLoginContent);
+      popup.open.bind(popup)();
+      popup.isOpen = true;
+      console.log(
+        "still got regLinks",
+        document.querySelectorAll(".popup__reg-link")
+      );
+    }
+  });
+  //////////
 
-authBtn.addEventListener('click', function() {
-  // console.log('got click')
-  if (!popup.isOpen){
-    popup.setContent(popupLoginContent);
-    popup.open.bind(popup)();
-    popup.isOpen = true;
-    console.log('still got regLinks', document.querySelectorAll('.popup__reg-link'));
-  }
-}
-);
-//////////
+  const popupRegContent = document.querySelector(
+    ".popup_registration__content"
+  );
 
-const popupRegContent = document.querySelector('.popup_registration__content');
-
-
-  template.addEventListener('click', function (event) {
-    if (event.target.classList.contains('popup__reg-link')) {
-    popup.close.bind(popup)();
+  template.addEventListener("click", function (event) {
+    if (event.target.classList.contains("popup__reg-link")) {
+      popup.close.bind(popup)();
       popup.setContent(popupRegContent);
       popup.open.bind(popup)();
       popup.isOpen = true;
     }
-});
+  });
 
+  // regLink.addEventListener('click', function() {
+  //   console.log('got click');
+  //   popup.close.bind(popup)();
+  //     popup.setContent(popupRegContent);
+  //     popup.open.bind(popup)();
+  //     popup.isOpen = true;
 
+  // }
+  // );
 
-
-
-// regLink.addEventListener('click', function() {
-//   console.log('got click');
-//   popup.close.bind(popup)();
-//     popup.setContent(popupRegContent);
-//     popup.open.bind(popup)();
-//     popup.isOpen = true;
-
-// }
-// );
-
-// authBtn.addEventListener('click', function() {console.log('bubu')});
-
-
-
+  // authBtn.addEventListener('click', function() {console.log('bubu')});
 })();
